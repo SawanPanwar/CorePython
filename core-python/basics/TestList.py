4676bf96c83a09921c2a6d94b79520c00332812d
SawanPanwar <skpanwar25@gmail.com>
2025-04-10 11:50:34 +0530

    10-04-2025

---
+++ core-python/basics/TestList.py
@@ -0,0 +1,9 @@
+list = [1, 'abc', True, 100.10]
+
+print('list =', list)
+
+list[0] = 100
+
+print('list =', list)
+
+print('length =', len(list))
---
+++ core-python/basics/TestVariables.py
@@ -0,0 +1,13 @@
+a = b = c = 10
+print('a =', a)
+print('b =', b)
+print('c =', c)
+
+a, b, c = 1, 2, 'abc'
+print('a =', a)
+print('b =', b)
+print('c =', c)
+
+a = 1
+b = 2
+c = 'abc'
