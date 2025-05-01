7ad3e3226b3cbaf919ec27f0677bf7cfd1a61bd9
SawanPanwar <skpanwar25@gmail.com>
2025-05-01 11:34:08 +0530

    01-05-2025

---
+++ core-python/file-io/copy_binary_file.py
@@ -0,0 +1,7 @@
+import shutil
+
+source = "../files/Rays.png";
+target = "../files/Sunrays.png";
+
+shutil.copyfile(source, target)
+print(source + " is copied to " + target)
---
+++ core-python/file-io/read_file.py
@@ -0,0 +1,9 @@
+def readFile():
+    file = open("../files/hello.txt", "r")
+    text = file.read()  # Read all data
+    print(text)
+    file.close()  # Close a file
+    return
+
+
+readFile()
